4fa7a4980079a54102ef5a63cfd56f79f938bc48
Leo Yu <plana@Leos-MacBook-Air.local>
2025-06-12 13:32:51 -0400

    feat: Migrate to Firebase Auth and fix chat streaming

---
+++ .next/static/chunks/src_app_(home)_page_tsx_90d0af88._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_32b3b0ba._.js"
+],
+    source: "dynamic"
+});
